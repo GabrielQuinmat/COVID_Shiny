a51040055b0dde6ec5b8f21a9ac39130c7ed07ee
Gabriel Quintanar <gquinmat@outlook.com>
2022-09-13 22:50:47 -0500

    Tab State almost readt

--- server.R
+++ server.R
@@ -16,7 +16,9 @@ library(leaflet)
 library(geojsonio)
 library(reactable)
 library(glue)
+library(stringr)
 library(htmltools)
+library(purrr)
 
 source("utils.R")
 
@@ -43,11 +45,13 @@ homeServer <- function(id) {
           add_bars(y = ~casosAcumulado, marker = list(color = gradient_palette[2]), name = "Acumulated Cases") %>%
           layout(
             barmode = "stack",
-            xaxis = list(title = "Time"),
-            yaxis = list(title = ""),
-            legend = list(x = 0, y = 1),
+            xaxis = list(title = "Time", color = "#ffffff"),
+            yaxis = list(title = "", color = "#ffffff"),
+            legend = list(x = 0, y = 1, font = list( color = "#ffffff")),
             title = "",
-            margin = list(b = 10, r = 20, l = 20, t = 50)
+            margin = list(b = 10, r = 20, l = 20, t = 20),
+            paper_bgcolor = "#101010",
+            plot_bgcolor = "#101010"
           )
       })
 
@@ -216,7 +220,171 @@ homeServer <- function(id) {
   )
 }
 
+stateServer <- function(id) {
+  moduleServer(
+    id, 
+    function(input, output, session){
+      rm(ufDF, totalDF, mergedDF)
+      geomun <- geojson_read("data/brasil_mun.geojson", what = "sp")
+      munDF <- dbData.table(conn, glue("
+          select
+            codmun, casosAcumulado, obitosAcumulado
+          from granular
+          where data == (
+            select max(data)
+            from granular
+            );"))
+
+      output$stateMap <- renderLeaflet({
+        geomun@data <- data.table(geomun@data)
+        geomun@data[, ":="(id_ord = 1:.N, id = as.numeric(id), codmun = map_chr(id, ~str_sub(., 1, 6)) %>% as.numeric())]
+
+        mergedDT <- merge(geomun@data, munDF,
+          by = c("codmun"), all.x = T
+        )
+
+        mergedDT[, CT := norm_vector(casosAcumulado, min(casosAcumulado)), .(UF)]
+        setorder(mergedDT, id_ord)
+        geomun@data <- mergedDT
+        geomun <- geomun[geomun$UF == input$stateSelect,]
+
+        colorpal <- colorNumeric(gradient_palette, c(0, geomun@data[, max(casosAcumulado)]))
+
+        labels <- glue("
+        <strong style = 'font-size: 13px'>{geomun@data[, NOME_ESTADO]} -
+        {geomun@data[, name]}</strong><br>
+        <span style = 'font-size: 11px; text-align: center; color: red'>{geomun@data[,
+                         format(round(casosAcumulado, 2),
+                         big.mark = ',')]}</span>
+                           ") %>%
+          lapply(htmltools::HTML)
+
+        leaflet(geomun, options = leafletOptions(zoomControl = F)) %>%
+          addProviderTiles(providers$CartoDB.DarkMatter,
+            options = providerTileOptions(minZoom = 4, maxZoom = 15)
+          ) %>%
+          addPolygons(
+            stroke = FALSE, smoothFactor = 0.5,
+            fillOpacity = 0.70, weight = 1,
+            color = ~ color_func(CT),
+            label = labels, dashArray = "3",
+            highlightOptions = highlightOptions(
+              weight = 2, dashArray = "",
+              color = "white",
+              fillOpacity = 1,
+              bringToFront = TRUE
+            )
+          ) %>%
+          addLegend("bottomright",
+            values = ~casosAcumulado,
+            title = "",
+            pal = colorpal,
+            opacity = 1
+          )
+      })
+
+      output$stateTable <- renderReactable({
+        latest.day <- dbData.table(conn, glue("
+          select
+            max(data) as fecha
+          from granular
+          ;")) %>% .[, fecha := as.Date(fecha, origin = "1970-01-01")]
+          
+
+        ufDF <- dbData.table(conn, glue("
+          select
+            *
+          from granular
+          where (data == {as.numeric(latest.day$fecha - 7)}
+          or data == {as.numeric(latest.day$fecha)})
+          and estado == '{input$stateSelect}';")) %>% 
+          .[, fecha := as.Date(data, origin = "1970-01-01")]
+        
+
+        df <- dcast(ufDF, estado + municipio ~ fecha, value.var = c("casosAcumulado", "obitosAcumulado"))
+        setnames(df, c("State", "Counties", "Cases_Last", "Cases_Current", "Defunctions_Last", "Defunctions_Current"))
+
+        df[, `:=`(
+          Cases_DIFF = Cases_Current - Cases_Last,
+          Defunctions_DIFF = Defunctions_Current - Defunctions_Last,
+          CurrentCases_Status = fifelse(Cases_Current - Cases_Last > 0, "Growing", "Decreasing"),
+          CurrentDefunc_Status = fifelse(Defunctions_Current - Defunctions_Last > 0, "Growing", "Decreasing")
+          )]
+
+        setorder(df, -Cases_DIFF)
+
+        reactable(
+          df[, .(Counties, Cases_Current, Defunctions_Current)],
+          fullWidth = T,
+          resizable = T,
+          highlight = T,
+          defaultPageSize = 16,
+          columns = list(
+            Counties = colDef(
+              name = "Counties",
+              align = "left",
+              width = 280,
+              cell = function(value, index) {
+                if (df[index, CurrentCases_Status] == "Growing") {
+                  ico <- "chevron circle up"
+                  className <- 'state-up'
+                } else {
+                  ico <- "chevron circle down"
+                  className <- 'state-down'
+                }
+
+                tagList(
+                  span(icon(ico), class = className),
+                  span(value, class = "state-name")
+                )
+              }
+            ),
+            Cases_Current = colDef(
+              name = "Cases",
+              align = "left",
+              format = colFormat(separators = T, digits = 0),
+              cell = function(value, index){
+                if (df[index, CurrentCases_Status] == "Growing") {
+                  className <- 'state-up-text'
+                } else {
+                  className <- 'state-down-text'
+                }
+
+                tagList(
+                  span(format(value, big.mark=","), class = "state-cases"),
+                  span('  '),
+                  span(df[index, Cases_DIFF], ' New Cases', class = className)
+                )
+                }
+              ),
+            Defunctions_Current = colDef(
+              name = "Defunctions",
+              align = "left",
+              format = colFormat(separators = T, digits = 0),
+              cell = function(value, index){
+                if (df[index, CurrentCases_Status] == "Growing") {
+                  className <- 'state-up-text'
+                } else {
+                  className <- 'state-down-text'
+                }
+
+                tagList(
+                  span(format(value, big.mark=","), class = "state-cases"),
+                  span('  '),
+                  span(df[index, Cases_DIFF], ' New', class = className)
+                )
+                }
+              )
+          )
+        )
+
+    })
+    })
+}
+
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
   homeServer("homeTab")
+  stateServer("stateTab")
 })
--- ui.R
+++ ui.R
@@ -12,61 +12,101 @@ library(semantic.dashboard)
 library(plotly)
 library(leaflet)
 library(reactable)
+library(lubridate)
 
 homeUI <- function(id) {
   ns <- NS(id)
   tagList(
-    h1('Country Level', class='tab-content__title'),
+    h1("Country Level", class = "tab-content__title"),
     fluidRow(
       box(
-        title='Acumulated Cases Through Time',
-        collapsible = F, ribbon = F, title_side = 'top',
+        title = "Acumulated Cases Through Time",
+        collapsible = F, ribbon = F, title_side = "top",
         plotlyOutput(ns("totalHistoric")),
         width = 16
       )
     ),
     fluidRow(
       box(
-        title='Acumulated Cases Today', 
-        collapsible = F, ribbon = F, title_side = 'top',
+        title = "Acumulated Cases Today",
+        collapsible = F, ribbon = F, title_side = "top",
         leafletOutput(ns("totalMap"), width = "100%", height = 600)
-      ) ,
-      box( 
-        title='Top States with Higher Amount of Cases', 
-        collapsible = F, ribbon = F, title_side = 'top',
-          reactableOutput(ns("totalTable"), width='100%', height = 600))
+      ),
+      box(
+        title = "Top States with Higher Amount of Cases",
+        collapsible = F, ribbon = F, title_side = "top",
+        reactableOutput(ns("totalTable"), width = "100%", height = 600)
+      )
+    )
+  )
+}
+
+stateUI <- function(id) {
+  ns <- NS(id)
+  tagList(
+    h1("State Level", class = "tab-content__title"),
+    selectInput(ns("stateSelect"), "Select a State", 
+    c("RO", "AC", "AM", "RR", "PA", "AP", "TO", "MA", 
+    "PI", "CE", "RN", "PB", "PE", "AL", "SE", "BA", "MG",
+    "ES", "RJ", "SP", "PR", "SC", "RS", "MS", "MT", "GO",
+    "DF"), selected="RO"),
+    fluidRow(
+      box(
+        title = "Acumulated Cases Today",
+        collapsible = F, ribbon = F, title_side = "top",
+        width = 16,
+        leafletOutput(
+          ns("stateMap"),
+          width = "100%"
+        )
+      )
+    ),
+    fluidRow(
+      box(
+        title = "Counties with Higher Amount of Cases",
+        collapsible = F, ribbon = F, title_side = "top",
+        reactableOutput(ns("stateTable"), width = "100%", height = 600),
+        width = 16
+      )
     )
   )
 }
 
 
 ui <- dashboardPage(
-  sidebar_and_body_container_class = 'class2',
+  sidebar_and_body_container_class = "class2",
   dashboardHeader(title = "Brazil COVID Cases"),
   dashboardSidebar(
-    class = 'dash-sidebar',
+    class = "dash-sidebar",
     sidebarMenu(
-    menuItem(
-      tabName = "home",
-      text = "General",
-      icon = icon("home")
-    ),
-    menuItem(
-      tabName = "state",
-      text = "State",
-      icon = icon("landmark")
+      menuItem(
+        tabName = "home",
+        text = "General",
+        icon = icon("home")
+      ),
+      menuItem(
+        tabName = "state",
+        text = "State",
+        icon = icon("landmark")
+      )
     )
-  )),
-  dashboardBody(
-    class = 'asdasd',
-    tabItems(
-    tabItem(tabName = 'home',
-            homeUI('homeTab')),
-    tabItem(tabName = 'state')
-  )),
-  tags$head(
-    tags$link(rel='stylesheet', type = 'text/css', href = 'style.css')
   ),
+  dashboardBody(
+    class = "asdasd",
+    tabItems(
+      tabItem(
+        tabName = "home",
+        homeUI("homeTab")
+      ),
+      tabItem(
+        tabName = "state",
+        stateUI("stateTab")
+      )
+    ),
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+    ),
+  )
 )
 
 # Define UI for application that draws a histogram
